a319584ea2342f0d934e446cf8c8734789e25f8f
fultux <flavien.rugiano@suse.com>
2023-02-17 10:39:04 +0100

    variabilization for subscription

--- resource-group/provider.tf
+++ resource-group/provider.tf
@@ -10,4 +10,8 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  client_id       = var.client_id 
+  client_secret   = var.secret
+  tenant_id       = var.tenant
+  subscription_id = var.subscription
 }
---
+++ resource-group/variables.tf
@@ -0,0 +1,20 @@
+variable "tenant" {
+  type    = string
+  description = "Azure's service principal Tenant name"
+}
+
+variable "secret" {
+  type    = string
+  description = "Azure's service principal secret"
+}
+
+variable "subscription" {
+  type    = string
+  description = "Azure's service principal subscription"
+}
+
+
+variable "client_id" {
+  type    = string
+  description = "Azure's service principal client id"
+}
